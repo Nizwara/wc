47b067f5c49f54b8d30190882dcb20b629f4c971
Nizwara <129714834+Nizwara@users.noreply.github.com>
2025-03-06 03:23:12 +0700

    Update wcf.py

--- wcf.py
+++ wcf.py
@@ -1,46 +1,33 @@
 import subprocess
 import os
-import sys
 
 def main():
     # Membersihkan layar terminal
     os.system('clear' if os.name == 'posix' else 'cls')
 
-    # URL file install.sh
-    url = "https://raw.githubusercontent.com/Nizwara/wcx/main/install.sh"
-    output_file = "install.sh"
+    # Clone repositori
+    print("Mengunduh direktori...")
+    result = subprocess.run(['git', 'clone', 'https://github.com/Nizwara/wcx.git'])
+    if result.returncode != 0:
+        print("Gagal mengunduh direktori. Pastikan URL benar dan koneksi internet stabil.")
+        exit(1)
 
-    # Unduh install.sh
-    print("Mengunduh install.sh...")
-    try:
-        subprocess.run(['wget', '-O', output_file, '-q', url], check=True)
-    except subprocess.CalledProcessError:
-        print("Gagal mengunduh install.sh. Pastikan URL benar dan koneksi internet stabil.")
-        sys.exit(1)
+    # Pindah ke direktori yang diunduh
+    os.chdir('wcx')
 
-    # Beri izin eksekusi pada install.sh
-    print("Memberikan izin eksekusi pada install.sh...")
-    try:
-        subprocess.run(['chmod', '+x', output_file], check=True)
-    except subprocess.CalledProcessError:
-        print("Gagal memberikan izin eksekusi pada install.sh.")
-        sys.exit(1)
+    # Beri izin eksekusi pada menu
+    print("Memberikan izin eksekusi pada menu...")
+    result = subprocess.run(['chmod', '+x', 'menu'])
+    if result.returncode != 0:
+        print("Gagal memberikan izin eksekusi pada menu.")
+        exit(1)
 
-    # Jalankan install.sh
-    print("Menjalankan install.sh...")
-    try:
-        subprocess.run([f'./{output_file}'], check=True)
-    except subprocess.CalledProcessError:
-        print("Gagal menjalankan install.sh.")
-        sys.exit(1)
-
-    # Setelah install.sh selesai, jalankan ./menu
+    # Jalankan menu
     print("Menjalankan menu...")
-    try:
-        subprocess.run(['./wcx/menu'], check=True)  # Sesuaikan path ke menu
-    except subprocess.CalledProcessError:
+    result = subprocess.run(['./menu'])
+    if result.returncode != 0:
         print("Gagal menjalankan menu.")
-        sys.exit(1)
+        exit(1)
 
 if __name__ == "__main__":
     main()
